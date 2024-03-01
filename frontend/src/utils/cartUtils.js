fd765f3260e9f7d67ad024809fefe6bffa34275f
Youssef Lamari <youssef.lamari98@gmail.com>
2024-03-01 18:25:14 +0100

    Initial commit

---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,22 @@
+export const addDecimals = (num) => {
+  return Math.round((num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  // Calculate Item Price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+
+  // Calculate Shipping Price
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+  // Calculate Total Price
+  state.totalPrice = (
+    Number(state.itemsPrice) + Number(state.shippingPrice)
+  ).toFixed(2);
+
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
+};
